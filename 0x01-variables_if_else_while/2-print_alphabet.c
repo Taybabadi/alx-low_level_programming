f39e2d95860b7fac7212219c3961b0c8d3e9aed6
Taybabadi <weaaam_123@hotmail.com>
2022-10-14 14:19:54 -0700

    task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 char low;
-for(low = 'a'; low <= 'z'; low++ )
+for (low = 'a'; low <= 'z'; low++)
 putchar(low);
-putchar('\n');  
+putchar('\n');
 return (0);
 }
