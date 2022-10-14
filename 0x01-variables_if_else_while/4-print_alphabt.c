eff3d84d082ef15e08e382bc34b7073088f1a1e1
Taybabadi <weaaam_123@hotmail.com>
2022-10-14 14:38:15 -0700

    task 6

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,19 +6,16 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
-{  
-char low, e, q;  
+int main(void)
+{
+char low, e, q;
 e = 'e';
-q = 'q';  
-for (low = 'a'; low <= 'z'; low++)    
-{      
-if (low != e && low != q)	
-putchar(low);      
-}  
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
 putchar('\n');
-  
-
-  
-return (0); 
+return (0);
 }
