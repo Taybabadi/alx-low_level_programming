a9f8fcc93ca710badf5ecc02a05468f43891d8b4
Taybabadi <weaaam_123@hotmail.com>
2022-10-14 13:09:15 -0700

    2task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,9 +7,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-  
 int n; 
 srand(time(0));
 n = rand() - RAND_MAX / 2;  
@@ -21,3 +19,4 @@ else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 return (0);
 }
+
